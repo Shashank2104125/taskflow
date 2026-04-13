b942799cb872cacfb417a3659aa4fd1a96efe6c5
Shashank Yadav <97789487+Shashank2104125@users.noreply.github.com>
2026-04-14 02:33:20 +0530

    handle migration and docker setup

--- src/main/resources/db/migration/V2__seed_data.sql
+++ src/main/resources/db/migration/V2__seed_data.sql
@@ -1,49 +1,71 @@
--- Fixed IDs (important)
--- User
-INSERT INTO users (id, name, email, password)
+-- USERS
+INSERT INTO users (id, name, email, password, created_at)
 VALUES (
     '11111111-1111-1111-1111-111111111111',
     'Test User',
     'test@example.com',
-    '$2a$10$Dow1p6v2vZl8Q0sP0XQ8QeF9uWcYQz8rF7kRXKuX3sI5Yucs5cjG6'
+    '$2a$10$uobb17WHxJS90186lcNWZOVPaVWetxASLQ4Ga3yVi.ddsioGFyNwm',
+    NOW()
 );
 
--- Project
-INSERT INTO projects (id, name, description, owner_id)
+-- PROJECTS
+INSERT INTO projects (id, name, description, owner_id, created_at)
 VALUES (
     '22222222-2222-2222-2222-222222222222',
     'Sample Project',
     'Seeded project for testing',
-    '11111111-1111-1111-1111-111111111111'
+    '11111111-1111-1111-1111-111111111111',
+    NOW()
 );
 
--- Tasks
-INSERT INTO tasks (title, description, status, priority, project_id, assignee_id, creator_id)
+-- TASKS
+-- TASKS
+INSERT INTO tasks (
+    id,
+    title,
+    description,
+    status,
+    priority,
+    project_id,
+    assignee_id,
+    creator_id,
+    created_at,
+    updated_at
+)
 VALUES
 (
+    '33333333-3333-3333-3333-333333333333',
     'Task 1',
     'Todo task',
     'todo',
     'low',
     '22222222-2222-2222-2222-222222222222',
     '11111111-1111-1111-1111-111111111111',
-    '11111111-1111-1111-1111-111111111111'
+    '11111111-1111-1111-1111-111111111111',
+    NOW(),
+    NOW()
 ),
 (
+    '44444444-4444-4444-4444-444444444444',
     'Task 2',
     'In progress task',
     'in_progress',
     'medium',
     '22222222-2222-2222-2222-222222222222',
     '11111111-1111-1111-1111-111111111111',
-    '11111111-1111-1111-1111-111111111111'
+    '11111111-1111-1111-1111-111111111111',
+    NOW(),
+    NOW()
 ),
 (
+    '55555555-5555-5555-5555-555555555555',
     'Task 3',
     'Done task',
     'done',
     'high',
     '22222222-2222-2222-2222-222222222222',
     '11111111-1111-1111-1111-111111111111',
-    '11111111-1111-1111-1111-111111111111'
+    '11111111-1111-1111-1111-111111111111',
+    NOW(),
+    NOW()
 );
